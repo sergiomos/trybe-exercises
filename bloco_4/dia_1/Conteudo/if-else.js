f1423834183d0fc86a5436aea8ed23aad7a71f51
Sérgio Martins <sergiomos2010@gmail.com>
2021-02-24 18:31:29 -0300

    Aula: Condicionais if else

---
+++ bloco_4/dia_1/Conteudo/if-else.js
@@ -0,0 +1,9 @@
+let grade = 59;
+
+if(grade >= 80) {
+  console.log('Parabéns você foi aprovada(o)!')
+}else if(grade >= 60){
+  console.log('Você está na nossa lista de espera')
+}else {
+  console.log('Você foi reprovada(o)')
+}
