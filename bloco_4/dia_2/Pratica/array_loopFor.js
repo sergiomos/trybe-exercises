e9bd5d0fb7b40b107ae68309cfe8d14fc80d0b8e
Sérgio Martins <sergiomos2010@gmail.com>
2021-02-26 10:03:19 -0300

    Exercicio pratico:Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado

--- bloco_4/dia_2/Pratica/array_loopFor.js
+++ bloco_4/dia_2/Pratica/array_loopFor.js
@@ -3,3 +3,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for(let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index])
 }
+
+let soma = null;
+
+for(let indexSoma = 0; indexSoma < numbers.length; indexSoma += 1){
+  soma += numbers[indexSoma]
+}
+
+console.log('A soma dos termos é igual a: ' + soma)
