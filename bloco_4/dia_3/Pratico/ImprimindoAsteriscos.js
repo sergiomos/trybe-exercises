e8bfd33c9a48b0f59047f958ad403fe896608e6e
Sérgio Martins <sergiomos2010@gmail.com>
2021-02-26 20:21:28 -0300

    Desafio 2 completo

--- bloco_4/dia_3/Pratico/ImprimindoAsteriscos.js
+++ bloco_4/dia_3/Pratico/ImprimindoAsteriscos.js
@@ -1,11 +1,25 @@
 // 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . 
 let n = 5;
-let squareSize = [];
+let square = [];
 
 for(let index = 0; index < n; index += 1) {
-  squareSize.push('*');
+  square.push('*');
 }
 
+console.log('Primeiro Desafio')
+
 for(let index = 0; index < n; index += 1) {
-  console.log(squareSize.join(''));
+  console.log(square.join(''));
 }
+
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+console.log('Segundo Desafio')
+
+let triangle = []
+
+for(let index = 0; index < n; index += 1) {
+  triangle.push('*');
+  console.log(triangle.join(''));
+}
+
