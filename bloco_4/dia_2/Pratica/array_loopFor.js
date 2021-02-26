f2bbd91365704777c22f8d219652365743b3f876
Sérgio Martins <sergiomos2010@gmail.com>
2021-02-26 10:08:03 -0300

    Exercicio pratico: Vendo se o ultimo elemento e maior que 20

--- bloco_4/dia_2/Pratica/array_loopFor.js
+++ bloco_4/dia_2/Pratica/array_loopFor.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 20];
 
 for(let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index])
@@ -15,3 +15,9 @@ console.log('A soma dos termos é igual a: ' + soma)
 let mediaAritmetica = soma / numbers.length;
 
 console.log('A média aritmética é igual a: ' + mediaAritmetica)
+
+if(numbers[numbers.length - 1] > 20){
+  console.log('Elemento final do array possui um valor maior que 20')
+}else{
+  console.log('Elemento final do array possui um valor menor ou igual a 20')
+}
