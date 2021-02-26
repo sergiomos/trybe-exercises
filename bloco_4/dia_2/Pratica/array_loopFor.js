ec2d1f13947760b581eaa37f5063e1b6a46f19e8
Sérgio Martins <sergiomos2010@gmail.com>
2021-02-26 11:20:03 -0300

    Exercicio pratico: encontrando o menor numero

--- bloco_4/dia_2/Pratica/array_loopFor.js
+++ bloco_4/dia_2/Pratica/array_loopFor.js
@@ -1,10 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 20];
 
 // 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
-
+console.log('##############################')
 for(let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index])
 }
+console.log('##############################')
+
 
 // 2. Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
 
@@ -16,11 +18,16 @@ for(let indexSoma = 0; indexSoma < numbers.length; indexSoma += 1){
 
 console.log('A soma dos termos é igual a: ' + soma)
 
+console.log('##############################')
+
+
 // 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 
 let mediaAritmetica = soma / numbers.length;
 
 console.log('A média aritmética é igual a: ' + mediaAritmetica)
+
+console.log('##############################')
 
 // 4.Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
@@ -29,6 +36,9 @@ if(numbers[numbers.length - 1] > 20){
 }else{
   console.log('Elemento final do array possui um valor menor ou igual a 20')
 } 
+
+console.log('##############################')
+
 
 // 5.Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 let maiorNumeroPontos = 0
@@ -47,6 +57,9 @@ for(let indexNumero = 0; indexNumero < numbers.length; indexNumero += 1) {
   maiorNumeroPontos = 0
 }
 
+console.log('##############################')
+
+
 
 // 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado"
 let quantosImpares = 0;
@@ -59,7 +72,27 @@ for(let indexMenor = 0; indexMenor < numbers.length; indexMenor += 1) {
 
 console.log('Temos ' + quantosImpares + ' numeros impares')
 
-// 7.
+console.log('##############################')
+
+// 7. Utilizando for , descubra qual o menor valor contido no array e imprima-o
+
+let menorNumeroPontos = 0
+
+for(let indexNumero = 0; indexNumero < numbers.length; indexNumero += 1) {
+
+  for(let indexComparacao = 0; indexComparacao < numbers.length; indexComparacao += 1) {
+    if(numbers[indexNumero] <= numbers[indexComparacao]){
+      maiorNumeroPontos += 1
+      if(maiorNumeroPontos === numbers.length){
+        console.log('O menor numero encontrado é o: ' + numbers[indexNumero])
+      }
+    }
+  }
+
+  maiorNumeroPontos = 0
+}
+console.log('##############################')
+
 
 // 8. Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado
 let newArray = []
@@ -68,6 +101,8 @@ for(let numberOfElements = 1; numberOfElements <= 25; numberOfElements += 1 ) {
 }
 
 console.log(newArray)
+
+console.log('##############################')
 
 // 9. Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
 
