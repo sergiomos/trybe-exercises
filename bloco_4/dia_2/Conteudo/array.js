7401e9123c405bbfab736d9e943426cbef4a206f
Sérgio Martins <sergiomos2010@gmail.com>
2021-02-26 09:54:20 -0300

    Conteudo: Adicione o valor "Contato" no final do array menu

--- bloco_4/dia_2/Conteudo/array.js
+++ bloco_4/dia_2/Conteudo/array.js
@@ -1,5 +1,8 @@
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
 let menuServices = menu[1];
 let indexOfPortfolio = menu.indexOf('Portfólio')
 
-console.log(menuServices, indexOfPortfolio);
+menu.push('Contato')
+
+console.log(menuServices, indexOfPortfolio, menu);
