62504e460dd81a5ca2dcb7e5347e5151a54996b0
Sérgio Martins <sergiomos2010@gmail.com>
2021-02-26 11:05:10 -0300

    Exercicio pratico: encontrando quantos impares tem

--- bloco_4/dia_2/Pratica/array_loopFor.js
+++ bloco_4/dia_2/Pratica/array_loopFor.js
@@ -1,8 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 20];
 
+// 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+
 for(let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index])
 }
+
+// 2. Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
 
 let soma = null;
 
@@ -12,12 +16,48 @@ for(let indexSoma = 0; indexSoma < numbers.length; indexSoma += 1){
 
 console.log('A soma dos termos é igual a: ' + soma)
 
+// 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
 let mediaAritmetica = soma / numbers.length;
 
 console.log('A média aritmética é igual a: ' + mediaAritmetica)
+
+// 4.Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
 if(numbers[numbers.length - 1] > 20){
   console.log('Elemento final do array possui um valor maior que 20')
 }else{
   console.log('Elemento final do array possui um valor menor ou igual a 20')
 }
+
+/* 5.Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+let maiorNumeroPontos = 0
+
+for(let indexLeitura = 0; indexLeitura < numbers.length; indexLeitura += 1) {
+
+  for(let indexComparacao = 0; indexComparacao < numbers.length; indexComparacao += 1) {
+    if(numbers[indexComparacao]  < numbers[indexLeitura]) {
+      maiorNumeroPontos += 1
+    }
+
+    if(maiorNumeroPontos === numbers.length){
+      console.log('O maior numero encontrado é o: ' + numbers[indexLeitura])
+    }
+
+    console.log('O numero:' + numbers[indexLeitura] + 'fez' + maiorNumeroPontos + 'pontos')
+  }
+
+  maiorNumeroPontos = 0
+}
+*/
+
+// 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado"
+let quantosImpares = 0;
+
+for(let indexMenor = 0; indexMenor < numbers.length; indexMenor += 1) {
+  if(numbers[indexMenor] % 2 !== 0) {
+    quantosImpares += 1;
+  }
+} 
+
+console.log('Temos ' + quantosImpares + ' numeros impares')
