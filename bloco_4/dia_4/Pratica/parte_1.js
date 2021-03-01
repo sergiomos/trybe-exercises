32fe6bb13fcfb3b040d8c9db5f17a8c83a5734c6
Sérgio Martins <sergiomos2010@gmail.com>
2021-03-01 16:07:09 -0300

    Pratica: Adicinado mais um objeto e printando ele

--- bloco_4/dia_4/Pratica/parte_1.js
+++ bloco_4/dia_4/Pratica/parte_1.js
@@ -4,9 +4,17 @@ let info = {
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
-console.log('Seja bem-vinda,' + ' ' + info.personagem);
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+};
 
 info['recorrente'] = 'Sim';
+info2['recorrente'] = 'Sim'
+
+console.log('Seja bem-vinda,' + ' ' + info.personagem);
+
 
 for(let index in info){
   console.log(index);
@@ -14,4 +22,13 @@ for(let index in info){
 
 for(let index in info){
   console.log(info[index]);
+}
+
+
+console.log(`${info.personagem} e ${info2.personagem}`);
+console.log(`${info.origem} e ${info2.origem}`);
+console.log(`${info.nota} e ${info2.nota}`);
+
+if(info.recorrente === 'Sim' && info2.recorrente === 'Sim'){
+  console.log('Ambos recorrentes')
 }
