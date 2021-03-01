ea84a9d5a5d7f8d03dab296ea73f4d6805ae0da0
Sérgio Martins <sergiomos2010@gmail.com>
2021-03-01 16:00:26 -0300

    Pratica: mostrando as chaves e valores

--- bloco_4/dia_4/Pratica/parte_1.js
+++ bloco_4/dia_4/Pratica/parte_1.js
@@ -4,6 +4,14 @@ let info = {
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
-console.log('Seja bem-vinda,' + ' ' + info.personagem)
+console.log('Seja bem-vinda,' + ' ' + info.personagem);
 
-info['recorrente'] = 'Sim'
+info['recorrente'] = 'Sim';
+
+for(let index in info){
+  console.log(index);
+}
+
+for(let index in info){
+  console.log(info[index]);
+}
