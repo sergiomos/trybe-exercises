b41ab1eed6709fff5f397562479c4e8cc48b9224
Sergio Martins <sergiomos2010@gmail.com>
2021-04-05 11:34:04 -0300

    Ex6

---
+++ fundamentos-do-desenvolvimento-web/bloco_8/dia_3/map.js
@@ -0,0 +1,39 @@
+// Comparação entre loop for e map
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = [];
+
+for (let index = 0; index < persons.length; index += 1) {
+  fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+}
+
+const fullNames2 = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+console.log(fullNames); // loop for
+console.log(fullNames2); // map
+
+//-------------------------------------------------------------------------------------
+// Exemplo 1:
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+// Exemplo 2:
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+    { [product]: listPrices[index] }
+  ));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
