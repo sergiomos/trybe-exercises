43c6226df027c0af935acb4ed44042a4328ffc27
Sergio Martins <sergiomos2010@gmail.com>
2021-04-07 07:11:20 -0300

    Exemplos de desestruturaçao de array

--- fundamentos-do-desenvolvimento-web/bloco_8/dia_5/objectDestructuring.js
+++ fundamentos-do-desenvolvimento-web/bloco_8/dia_5/objectDestructuring.js
@@ -21,3 +21,16 @@ const { a: name, b: classAssigned, c: subject } = student;
 console.log(name); // Maria
 console.log(classAssigned); // Turma B
 console.log(subject); // Matemática
+
+//Exemplo 3:
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+const printProductDetails = ({ name, price, seller }) => {
+  console.log(`Promoção! ${name} por apenas ${price} é só aqui: ${seller}`);
+};
+
+printProductDetails(product); // Promoção! Smart TV Crystal UHD por apenas 1899.05 é só aqui: Casas de Minas
