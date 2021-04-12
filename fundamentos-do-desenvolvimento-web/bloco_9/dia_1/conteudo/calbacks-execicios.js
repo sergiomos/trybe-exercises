c7ebe702feed87d71c3a17e1e4ac9773d1c0d328
Sergio Martins <sergiomos2010@gmail.com>
2021-04-12 07:18:55 -0300

    Conteudo do dia 9.1

---
+++ fundamentos-do-desenvolvimento-web/bloco_9/dia_1/conteudo/calbacks-execicios.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+
+// Exercicio de fixação 1
+/*
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn);
+};
+
+assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser */
+
+
+// Exercicio de fixação 2
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    console.log(callback(user));
+  }, delay());
+};
+
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
