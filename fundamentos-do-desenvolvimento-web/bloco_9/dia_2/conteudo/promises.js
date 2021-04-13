e4a6c5e9470e5b97e4f5987640cfad961c3fff71
Sergio Martins <sergiomos2010@gmail.com>
2021-04-13 07:58:00 -0300

    Conteudo do dia 9.2

---
+++ fundamentos-do-desenvolvimento-web/bloco_9/dia_2/conteudo/promises.js
@@ -0,0 +1,22 @@
+// Estados de promises: pending, resolve, reject
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+  }
+  resolve(console.log(`Que sucesso =) nosso número foi ${number}`));
+});
+
+// Gestores de fluxo
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(number);
+  }
+  resolve(number);
+})
+.then(number => `Que sucesso =) nosso número foi ${number}`)
+.then(msg => console.log(msg))
+.catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
